27778bb911668c09f4a528f4c1477541241a2a81
Akklom <100361279+Akklom@users.noreply.github.com>
2022-03-28 17:03:42 +0200

    reworked characterselect

--- Scripts/Globals/CharacterSave.gd
+++ Scripts/Globals/CharacterSave.gd
@@ -2,23 +2,25 @@ extends Node
 var ingame := false
 var first_spawn := true
 var controller := false
-var save_dict = {"character_name" : "character_name", "ironman" : false, "current_checkpoint" : 0, 
+var profile := 1
+var save_dict = {"ironman" : false, "current_checkpoint" : 0, 
 					"walljump": false, "doublejump": false, "gun" : false, "flashlight": false, "timeslow": false, "crawling": false, "running": false,
 					"orange_keycard": false}
 
 
 func save_progress():
 	var savefile = File.new()
-	savefile.open("user://save_file"+ save_dict["character_name"] +".dat", File.WRITE)
+	savefile.open("user://save_file"+ str(profile) +".dat", File.WRITE)
 	savefile.store_var(save_dict)
 	savefile.close()
+	
 	
 	
 func load_progress(loadeddata):
 	save_dict = loadeddata
 	
 	
-func delete_save():
-	save_dict = {"character_name" : save_dict["character_name"], "ironman" : false, "current_checkpoint" : 0, 
-					"walljump": false, "doublejump": false, "gun" : false, "flashlight": false, "timeslow": false, "crawling": false, "running": false}
-	save_progress()
+#func delete_save():
+#	save_dict = {"character_name" : save_dict["character_name"], "ironman" : false, "current_checkpoint" : 0, 
+#					"walljump": false, "doublejump": false, "gun" : false, "flashlight": false, "timeslow": false, "crawling": false, "running": false}
+#	save_progress()
--- Scripts/UI Script/Characterselect.gd
+++ Scripts/UI Script/Characterselect.gd
@@ -1,211 +1,142 @@
 extends Control
-var ironman := false
-var delete_array = []
-var charslot_dict = {"Charslot1": ["empty", "empty"], "Charslot2" : ["empty", "empty"], "Charslot3" : ["empty", "empty"] }
-var progress_dict = {}
-var pressed_loading := false
+var progress_dict1 := {"ironman" : false, "current_checkpoint" : 0, 
+					"walljump": false, "doublejump": false, "gun" : false, "flashlight": false, "timeslow": false, "crawling": false, "running": false,
+					"orange_keycard": false}
+var progress_dict2 := {"ironman" : false, "current_checkpoint" : 0, 
+					"walljump": false, "doublejump": false, "gun" : false, "flashlight": false, "timeslow": false, "crawling": false, "running": false,
+					"orange_keycard": false}
+var progress_dict3 := {"ironman" : false, "current_checkpoint" : 0, 
+					"walljump": false, "doublejump": false, "gun" : false, "flashlight": false, "timeslow": false, "crawling": false, "running": false,
+					"orange_keycard": false}
 
-const ironman_texture = preload("res://icon.png")
-const bunny_texture = preload("res://icon.png")
+var delet_save:= "1"
+var PressedLoading := false
+
 func _ready():
-	LoadSlots()
-	#$N/H/LineEdit.grab_focus()
-	$N/H1/IronmanMode/PlayChar1.grab_focus()
-
-func SaveSlots():
+		$N/H1/PlayChar1.grab_focus()
+		progress_load()
+		
+func progress_load():
 	var savefile = File.new()
-	savefile.open("user://gamedata.dat", File.WRITE)
-	savefile.store_var(charslot_dict)
-	savefile.close()
-
-func LoadSlots():
-	var savefile = File.new()
-	if savefile.file_exists("user://gamedata.dat"):
-		savefile.open("user://gamedata.dat", File.READ)
-		charslot_dict = savefile.get_var()
-		savefile.close()
-		CharacterSlots()
+	if savefile.file_exists("user://save_file1.dat"):
+		$N/H1/PlayChar1/Charactername.text = "Profile 1"
+		$N/H1/Delete1.visible = true
 	else:
-		savefile.open("user://gamedata.dat", File.WRITE)
-		savefile.store_var(charslot_dict)
-		savefile.close()
-		
-		
-func _on_IronmanMode_toggled(button_pressed):
-	if button_pressed:
-		ironman = true
-		get_node("N/H1/IronmanMode/PlayChar1").texture_normal = ironman_texture
-		get_node("N/H2/IronmanMode/PlayChar2").texture_normal = ironman_texture
-		get_node("N/H3/IronmanMode/PlayChar3").texture_normal = ironman_texture
+		$N/H1/PlayChar1/Charactername.text = "-empty-"
+		$N/H1/Delete1.visible = false
+	if savefile.file_exists("user://save_file2.dat"):
+		$N/H2/PlayChar2/Charactername.text = "Profile 2"
+		$N/H2/Delete2.visible = true
 	else:
-		ironman = false
-		get_node("N/H1/IronmanMode/PlayChar1").texture_normal = bunny_texture
-		get_node("N/H2/IronmanMode/PlayChar2").texture_normal = bunny_texture
-		get_node("N/H3/IronmanMode/PlayChar3").texture_normal = bunny_texture
-		
-func _on_LineEdit_text_entered(new_text):
-	#var counter = 1
-	var savefile = File.new()
-	for character in charslot_dict:
-		if charslot_dict[character][0] == "empty" and new_text != "empty":
-			#savefile.open("user://save_file"+ new_text +".dat", File.WRITE)
-			#savefile.store_var(CharacterSave.save_dict)
-			charslot_dict[character][0] = new_text
-			if ironman == true:
-				charslot_dict[character][1] = "true"
-			else:
-				charslot_dict[character][1] = "false"
-			CharacterSlots()
-			#counter += 1
-			SaveSlots()
-			break
+		$N/H2/PlayChar2/Charactername.text = "-empty-"
+		$N/H2/Delete2.visible = false
+	if savefile.file_exists("user://save_file3.dat"):
+		$N/H3/PlayChar3/Charactername.text = "Profile 3"
+		$N/H3/Delete3.visible = true
+	else:
+		$N/H3/PlayChar3/Charactername.text = "-empty-"
+		$N/H3/Delete3.visible = false
+
 	
-	
-func CharacterSlots():
-	var slot = 1
-	for character in charslot_dict:
-		match character:
-			"Charslot1":
-					slot = 1
-			"Charslot2":
-					slot = 2
-			"Charslot3":
-					slot = 3
-		if charslot_dict[character][1] != "empty":
-			get_node("N/H"+ str(slot) +"/Charactername").text = charslot_dict[character][0]
-			get_node("N/H"+ str(slot) +"/Label").text =  "   Play ->"
-			get_node("N/H"+ str(slot) +"/Delete"+str(slot)).visible = true
-			get_node("N/H"+ str(slot) +"/IronmanMode").visible = true
-			#if charslot_dict[character][1] == "true":
-				#get_node("N/H"+ str(slot) +"/IronmanMode/IronmanTexture").texture = ironman_texture
-			#elif charslot_dict[character][1] == "false":
-			#	get_node("N/H"+ str(slot) +"/IronmanMode/IronmanTexture").texture = bunny_texture
-			#slot += 1
-		else:
-			get_node("N/H"+ str(slot) +"/Delete"+str(slot)).visible = false
-			get_node("N/H"+ str(slot) +"/IronmanMode").visible = false
-			
 func _on_PlayChar1_pressed():
-	if pressed_loading == false:
-		pressed_loading = true
-		if charslot_dict["Charslot1"][0] != "empty":
-			var savefile = File.new()
-		
-			if savefile.file_exists("user://save_file"+ charslot_dict["Charslot1"][0] +".dat"):
-				savefile.open("user://save_file"+ charslot_dict["Charslot1"][0] +".dat", File.READ)
-				progress_dict = savefile.get_var()
-				CharacterSave.load_progress(progress_dict)
-				savefile.close()
-				$SFXStreamPlayer.play()
-				yield(get_tree().create_timer(0.5), "timeout")
-				get_tree().change_scene("res://Scenes/Maps/Lighttest.tscn")
-			else:
-				savefile.open("user://save_file"+ charslot_dict["Charslot1"][0] +".dat", File.WRITE)
-				savefile.close()
-				CharacterSave.save_dict["character_name"] = charslot_dict["Charslot1"][0]
-				CharacterSave.save_dict["ironman"] = charslot_dict["Charslot1"][1]
-				CharacterSave.save_progress()
-				$SFXStreamPlayer.play()
-				yield(get_tree().create_timer(0.5), "timeout")
-				get_tree().change_scene("res://Scenes/Maps/Lighttest.tscn")
-
+	var savefile = File.new()
+	if savefile.file_exists("user://save_file1.dat"):
+		if PressedLoading == false:
+			PressedLoading = true
+			CharacterSave.profile = 1
+			savefile.open("user://save_file1.dat", File.READ)
+			CharacterSave.load_progress(savefile.get_var())
+			savefile.close()
+			$SFXStreamPlayer.play()
+			$N/H1/PlayChar1.texture_focused = load("res://Assets/UI/Buttons/Button_Clicked.png")
+			yield(get_tree().create_timer(0.5), "timeout")
+			get_tree().change_scene("res://Scenes/Maps/Lighttest.tscn")
+	else:
+		var savefile1 = File.new()
+		savefile1.open("user://save_file1.dat", File.WRITE)
+		savefile1.store_var(progress_dict1)
+		savefile1.close()
+		progress_load()
 
 func _on_PlayChar2_pressed():
-	if pressed_loading == false:
-		pressed_loading = true
-		if charslot_dict["Charslot2"][0] != "empty":
-			var savefile = File.new()
-			if savefile.file_exists("user://save_file"+ charslot_dict["Charslot2"][0] +".dat"):
-				savefile.open("user://save_file"+ charslot_dict["Charslot2"][0] +".dat", File.READ)
-				progress_dict = savefile.get_var()
-				CharacterSave.load_progress(progress_dict)
-				savefile.close()
-				$SFXStreamPlayer.play()
-				yield(get_tree().create_timer(0.5), "timeout")
-				get_tree().change_scene("res://Scenes/Maps/Lighttest.tscn")
-			else:
-				savefile.open("user://save_file"+ charslot_dict["Charslot2"][0] +".dat", File.WRITE)
-				savefile.close()
-				CharacterSave.save_dict["character_name"] = charslot_dict["Charslot2"][0]
-				CharacterSave.save_dict["ironman"] = charslot_dict["Charslot2"][1]
-				CharacterSave.save_progress()
-				$SFXStreamPlayer.play()
-				yield(get_tree().create_timer(0.5), "timeout")
-				get_tree().change_scene("res://Scenes/Maps/Lighttest.tscn")
+	var savefile = File.new()
+	if savefile.file_exists("user://save_file2.dat"):
+		if PressedLoading == false:
+			PressedLoading = true
+			CharacterSave.profile = 2
+			savefile.open("user://save_file2.dat", File.READ)
+			CharacterSave.load_progress(savefile.get_var())
+			savefile.close()
+			$SFXStreamPlayer.play()
+			$N/H2/PlayChar2.texture_focused = load("res://Assets/UI/Buttons/Button_Clicked.png")
+			yield(get_tree().create_timer(0.5), "timeout")
+			get_tree().change_scene("res://Scenes/Maps/Lighttest.tscn")
+	else:
+		savefile.open("user://save_file2.dat", File.WRITE)
+		savefile.store_var(progress_dict2)
+		savefile.close()
+		progress_load()
 
 func _on_PlayChar3_pressed():
-	if pressed_loading == false:
-		pressed_loading = true
-		if charslot_dict["Charslot3"][0] != "empty":
-			var savefile = File.new()
-			if savefile.file_exists("user://save_file"+ charslot_dict["Charslot3"][0] +".dat"):
-				savefile.open("user://save_file"+ charslot_dict["Charslot3"][0] +".dat", File.READ)
-				progress_dict = savefile.get_var()
-				CharacterSave.load_progress(progress_dict)
-				savefile.close()
-				$SFXStreamPlayer.play()
-				yield(get_tree().create_timer(0.5), "timeout")
-				get_tree().change_scene("res://Scenes/Maps/Lighttest.tscn")
-			else:
-				savefile.open("user://save_file"+ charslot_dict["Charslot3"][0] +".dat", File.WRITE)
-				savefile.close()
-				CharacterSave.save_dict["character_name"] = charslot_dict["Charslot3"][0]
-				CharacterSave.save_dict["ironman"] = charslot_dict["Charslot3"][1]
-				CharacterSave.save_progress()
-				$SFXStreamPlayer.play()
-				yield(get_tree().create_timer(0.5), "timeout")
-				get_tree().change_scene("res://Scenes/Maps/Lighttest.tscn")
-			
-			
-func _on_ConfirmDelete_pressed():
-	var dir = Directory.new()
-	dir.remove("user://save_file"+ delete_array[0] +".dat")
-	charslot_dict[delete_array[1]] = ["empty", "empty"]
-	$N/Popup.visible = false
-	SaveSlots()
+	var savefile = File.new()
+	if savefile.file_exists("user://save_file3.dat"):
+		if PressedLoading == false:
+			PressedLoading = true
+			CharacterSave.profile = 3
+			savefile.open("user://save_file3.dat", File.READ)
+			CharacterSave.load_progress(savefile.get_var())
+			savefile.close()
+			$SFXStreamPlayer.play()
+			$N/H3/PlayChar3.texture_focused = load("res://Assets/UI/Buttons/Button_Clicked.png")
+			yield(get_tree().create_timer(0.5), "timeout")
+			get_tree().change_scene("res://Scenes/Maps/Lighttest.tscn")
+	else:
+		savefile.open("user://save_file3.dat", File.WRITE)
+		savefile.store_var(progress_dict3)
+		savefile.close()
+		progress_load()
+func _on_Delete1_pressed():
+	$N/Popup.visible = true
+	$N/Popup/TextureRect/StopDelete.grab_focus()
+	delet_save = "1"
 	
-	if delete_array[1] == "Charslot1":
-		get_node("N/H1/Charactername").text = "                                   -  Empty Characterslot    -"
-		get_node("N/H1/IronmanMode/IronmanTexture").texture = null
-		get_node("N/H1/Label").text = ""
-		get_node("N/H1/Delete1").visible = false
-		get_node("N/H1/IronmanMode").visible = false
-	if delete_array[1] == "Charslot2":
-		get_node("N/H2/Charactername").text = "                                   -  Empty Characterslot    -"
-		get_node("N/H2/IronmanMode/IronmanTexture").texture = null
-		get_node("N/H2/Label").text = ""
-		get_node("N/H2/Delete2").visible = false
-		get_node("N/H2/IronmanMode").visible = false
-	if delete_array[1] == "Charslot3":
-		get_node("N/H3/Charactername").text = "                                   -  Empty Characterslot    -"
-		get_node("N/H3/IronmanMode/IronmanTexture").texture = null
-		get_node("N/H3/Label").text = ""
-		get_node("N/H3/Delete3").visible = false
-		get_node("N/H3/IronmanMode").visible = false
+func _on_Delete2_pressed():
+	$N/Popup.visible = true
+	$N/Popup/TextureRect/StopDelete.grab_focus()
+	delet_save = "2"
+
+
+func _on_Delete3_pressed():
+	$N/Popup.visible = true
+	$N/Popup/TextureRect/StopDelete.grab_focus()
+	delet_save = "3"
+
+
+func _on_PlayChar1_mouse_entered():
+	if PressedLoading == false:
+		$N/H1/PlayChar1.grab_focus()
+
+
+func _on_PlayChar2_mouse_entered():
+	if PressedLoading == false:
+		$N/H2/PlayChar2.grab_focus()
+
+
+func _on_PlayChar3_mouse_entered():
+	if PressedLoading == false:
+		$N/H3/PlayChar3.grab_focus()
+
+
+func _on_ConfirmDelete_pressed():
+	$N/Popup.visible = false
+	var dir = Directory.new()
+	dir.remove("user://save_file"+ delet_save +".dat")
+	progress_load()
+
+	
 
 
 func _on_StopDelete_pressed():
 	$N/Popup.visible = false
+	$N/H1/PlayChar1.grab_focus()
 
-
-func _on_Delete1_pressed():
-	$N/Popup/TextureRect/charname.text = charslot_dict["Charslot1"][0]
-	$N/Popup.visible = true
-	delete_array = [charslot_dict["Charslot1"][0], "Charslot1"]
-
-func _on_Delete2_pressed():
-	$N/Popup/TextureRect/charname.text = charslot_dict["Charslot2"][0]
-	$N/Popup.visible = true
-	delete_array = [charslot_dict["Charslot2"][0], "Charslot2"]
-
-func _on_Delete3_pressed():
-	$N/Popup/TextureRect/charname.text = charslot_dict["Charslot3"][0]
-	$N/Popup.visible = true
-	delete_array = [charslot_dict["Charslot3"][0], "Charslot3"]
-
-
-
-
-
-func _on_BackButton_pressed():
-	get_tree().change_scene("res://scenes/MainMenu.tscn")
